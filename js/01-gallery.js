42ec91d21e8b870f7019e78a8bdf203cfe6ce5f6
Taras <tarasbohun07@gmail.com>
2022-09-22 12:49:24 +0300

    done task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const itemTemplate = ({ preview, original, description }) =>
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+
+const renderGalleryItems = () => {
+  const list = galleryItems.map(itemTemplate).join("");
+
+  gallery.innerHTML = "";
+  gallery.insertAdjacentHTML("beforeend", list);
+};
+
+renderGalleryItems();
+
+gallery.addEventListener("click", onItemClick);
+
+function onItemClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const urlCurrentImage = e.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+        <img src = '${urlCurrentImage}' width = 960/>
+    </div>
+`);
+
+  instance.show();
+
+  window.addEventListener("keydown", onCloseModal);
+
+  function onCloseModal(e) {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  }
+}
