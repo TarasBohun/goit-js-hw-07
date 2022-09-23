2d3735d1eb32f5431cff8f51487725eaac2cbaf8
Taras <tarasbohun07@gmail.com>
2022-09-23 21:02:54 +0300

    done task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,37 +1,24 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// console.log(galleryItems);
-
 const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("beforeend", renderGalleryItems(galleryItems));
 
 function renderGalleryItems() {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `<a class="gallery__item" href="${original}">
+      return `
+      <a class="gallery__item" href="${original}">
                  <img class="gallery__image"
-                 src="${preview}" 
+                 src="${preview}"
                  alt="${description}" />
-</a>`;
+</a>
+`;
     })
     .join("");
 }
 
-gallery.innerHTML = renderGalleryItems();
-
-gallery.addEventListener("click", onImageClick);
-
-function onImageClick(e) {
-  e.preventDefault();
-  const isImage = e.target.classList.contains("gallery__image");
-  if (!isImage) {
-    return;
-  }
-  const imageAlt = e.target.alt;
-  console.log(imageAlt);
-
-  let lightbox = new SimpleLightbox(".gallery__item");
-  lightbox.on("show.simplelightbox", function () {});
-  //   console.log(e.currentTarget);
-  //   console.log(lightbox);
-}
+new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
